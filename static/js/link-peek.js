0b17df1c16a287f62cd9997914d8ac1f3fe1099c
hfa224 <helenffionadams@gmail.com>
2024-07-29 21:56:00 +0100

    Updates to fix null descriptions and default images

--- static/js/link-peek.js
+++ static/js/link-peek.js
@@ -41,11 +41,13 @@ class LinkPeek extends HTMLElement {
 
   populateSlot(slot, value) {
     if (typeof value == "string" && value.startsWith("http")) {
-      if (slot.localName === "a") slot.href = value;
-    } if (typeof value == "string" && slot.localName === "img") { // remove http check for image to allow default img
-      slot.src = value;
+      if (slot.localName === "a") { 
+        slot.href = value;
+      } else if (slot.localName === "img") {
+        slot.src = value;
+      }
     } else {
-      if (!value.startsWith("http")) slot.textContent = value;
+      slot.textContent = value;
     }
   }
 
