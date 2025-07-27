8695c6f6630b82dd7e849523dd2eb0bc31332bdc
hfa224 <helenffionadams@gmail.com>
2025-07-27 20:31:11 +0100

    #59 add pillow to requirements file, formatting and linting

--- postcards.py
+++ postcards.py
@@ -17,7 +17,7 @@ def get_postcards():
     print(onlyfiles)
     postcard_info_list = list(map(create_postcard_info, onlyfiles))
     for info in postcard_info_list:
-        for keys,values in info.items():
+        for keys, values in info.items():
             print(keys)
             print(values)
 
@@ -29,7 +29,7 @@ def create_postcard_info(filename):
     return {
         "front_url": filename,
         "back_url": filename.replace("front", "back"),
-        "orientation": get_orientation(filename)
+        "orientation": get_orientation(filename),
     }
 
 
@@ -43,25 +43,4 @@ def get_orientation(img_name):
 
     if size[0] > size[1]:
         return "h"
-    else:
-        return "v"
-
-    # print(f'im1.size={im1.size}')
-    # print(f'im2.size={im2.size}')
-
-    # for orientation in ExifTags.TAGS.keys():
-    #     if ExifTags.TAGS[orientation] == 'Orientation':
-    #         break
-
-    # print(f'orientation={orientation}')
-
-    # exif1 = dict(im1._getexif().items())
-    # #exif2 = dict(im2._getexif().items())
-
-    # # dump exif data
-    # exif = im1.getexif()
-    # for k, v in exif.items():
-    #    print('{}: {}'.format(ExifTags.TAGS[k], v))
-
-    # print(f'exif1 orientation={exif1[orientation]}')
-    # print(f'exif2 orientation={exif2[orientation]}')
+    return "v"
