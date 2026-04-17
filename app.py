6681516a768a05add44a42ca2f15883785ed58cc
hfa224 <helenffionadams@gmail.com>
2026-04-17 20:58:26 +0100

    refs #62 remove some old images, simplify photo_template.html and produce_photo_page method in app.py.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Serves up the personal site"""
 
-import re
+import re, os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -47,14 +47,14 @@ def counters():
 def photos_2025_07_03():
     """Renders photo page for 2025_07_03"""
     return produce_photo_page(
-        "2025_07_03", "0000087500", range(1, 36)
+        "2025_07_03"
     )
 
 @app.route("/2025_02_08/")
 def photos_2025_02_08():
     """Renders photo page for 2025_02_08"""
     return produce_photo_page(
-        "2025_02_08", "0000087500", [ 24, 33, 34, 35, 36, 37, 38, 39]
+        "2025_02_08"
         
     )
 
@@ -62,42 +62,49 @@ def photos_2025_02_08():
 def photos_2025_06_25():
     """Renders photo page for 2025_06_25"""
     return produce_photo_page(
-        "2025_06_25", "0000052000", range(1, 39)
+        "2025_06_25"
     )
 
 @app.route("/2025_06_25_2/")
 def photos_2025_06_25_2():
     """Renders photo page for 2025_06_25_2"""
     return produce_photo_page(
-        "2025_06_25_2", "0000052300", range(1, 40)
+        "2025_06_25_2"
     )
 
 @app.route("/2025_07_22/")
 def photos_2025_07_22():
     """Renders photo page for 2025_07_22"""
     return produce_photo_page(
-        "2025_07_22", "0000067600", list(range(1, 29)) + [39, 40]
+        "2025_07_22"
     )
 
 @app.route("/2025_09_06/")
 def photos_2025_09_06():
     """Renders photo page for 2025_09_06"""
     return produce_photo_page(
-        "2025_09_06", "0000082800", range(1, 41)
+        "2025_09_06"
     )
+
+@app.route("/2026_01_01/")
+def photos_2026_01_01():
+    """Renders photo page for 2026_01_01"""
+    return produce_photo_page(
+        "2026_01_01"
+)
 
 @app.route("/2026_03_09/")
 def photos_2026_03_09():
     """Renders photo page for 2026_03_09"""
     return produce_photo_page(
-        "2026_03_09", "0000", [1, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+        "2026_03_09"
     )
 
 @app.route("/2022_10_22/")
 def photos_2022_10_22():
     """Renders photo page for 2022_10_22"""
     return produce_photo_page(
-        "2022_10_22", "0000051500", [1, 2, 5, 7, 8, 9, 10, 11, 12, 15, 17, 18, 19, 20]
+        "2022_10_22"
     )
 
 
@@ -105,40 +112,7 @@ def photos_2022_10_22():
 def photos_2024_04_22():
     """Renders photo page for 2024_04_22"""
     return produce_photo_page(
-        "2024_04_22",
-        "0000050400",
-        [
-            1,
-            2,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-            16,
-            17,
-            18,
-            19,
-            20,
-            25,
-            26,
-            27,
-            28,
-            29,
-            30,
-            31,
-            32,
-            33,
-            34,
-            35,
-            36,
-        ],
+        "2024_04_22"
     )
 
 
@@ -146,9 +120,7 @@ def photos_2024_04_22():
 def photos_2024_07_21():
     """Renders photo page for 2024_07_21"""
     return produce_photo_page(
-        "2024_07_21",
-        "0000093500",
-        [2, 4, 5, 6, 7, 8, 11, 12, 13, 17, 18, 20, 21, 22, 23, 24, 25, 26],
+        "2024_07_21"
     )
 
 
@@ -156,43 +128,14 @@ def photos_2024_07_21():
 def photos_2024_08_06():
     """Renders photo page for 2024_08_06"""
     return produce_photo_page(
-        "2024_08_06",
-        "0000024500",
-        [
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            11,
-            12,
-            13,
-            14,
-            15,
-            16,
-            18,
-            19,
-            20,
-            22,
-            24,
-            25,
-            26,
-            27,
-            28,
-            29,
-        ],
+        "2024_08_06"
     )
 
 @app.route("/2024_09_27/")
 def photos_2024_09_27():
     """Renders photo page for 2024_09_27"""
     return produce_photo_page(
-        "2024_09_27",
-        "0000057600",
-        [1, 2, 4, 5, 6, 7, 9, 10, 11, 14, 15, 16, 20, 21, 22, 23, 24, 26, 27, 28],
+        "2024_09_27"
     )
 
 
@@ -200,12 +143,7 @@ def photos_2024_09_27():
 def photos_2024_12_01():
     """Renders photo page for 2024_12_01"""
     return produce_photo_page(
-        "2024_12_01",
-        "0000034800",
-        [1, 2, 4, 5, 6, 7, 11, 13,
-         14, 15, 16, 17, 18, 19,
-         20, 21, 22, 23, 24, 25,
-         26, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38],
+        "2024_12_01"
     )
 
 
@@ -221,10 +159,14 @@ def jjhagdo():
 
 
 def produce_photo_page(
-    photos_folder_name, photo_file_prefix, indexes_of_displayed_photos
+    photos_folder_name
 ):
     """Renders the photo template page for the photos in the given photos_folder_name"""
-    root_img_name = "images/" + photos_folder_name + "/" + photo_file_prefix
+    photo_folder_name = "images/" + photos_folder_name
+    image_list = []
+    for file in os.listdir(os.path.join("static", photo_folder_name)):
+        if "txt" not in file:
+            image_list.append(photo_folder_name +"/" + file)
     with open(
         "static/images/" + photos_folder_name + "/descriptions.txt", encoding="utf-8"
     ) as f:
@@ -232,8 +174,7 @@ def produce_photo_page(
     return render_template(
         "photo_template.html",
         photos_folder_name=photos_folder_name,
-        root_img_name=root_img_name,
-        indexes_of_displayed_photos=indexes_of_displayed_photos,
+        image_list=image_list,
         descriptions=lines,
     )
 
