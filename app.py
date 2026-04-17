50f4d3186b1dc5e606cdb7865c1eca65bf4966d4
hfa224 <helenffionadams@gmail.com>
2026-04-17 21:14:56 +0100

    Formatting with ruff

--- app.py
+++ app.py
@@ -8,20 +8,24 @@ app = Flask(__name__)
 
 app.config["FREEZER_RELATIVE_URLS"] = True
 
+
 @app.route("/")
 def landing_page():
     """Renders landing page"""
     return render_template("landing-page.html")
+
 
 @app.route("/about/")
 def landing_page_about():
     """Renders about page"""
     return render_template("landing-page-about.html")
 
+
 @app.route("/changelog/")
 def landing_page_changelog():
     """Renders changelog page"""
     return render_template("changelog.html")
+
 
 @app.route("/colophon/")
 def landing_page_colophon():
@@ -34,128 +38,110 @@ def sheets():
     """Renders sheets page"""
     return render_template("sheets/book_log.html")
 
+
 @app.route("/photos/")
 def photos():
     """Renders base photo page"""
     return render_template("photos.html")
+
 
 @app.route("/counters/")
 def counters():
     """Renders counters page"""
     return render_template("archive/counters.html")
 
+
 @app.route("/2025_07_03/")
 def photos_2025_07_03():
     """Renders photo page for 2025_07_03"""
-    return produce_photo_page(
-        "2025_07_03"
-    )
+    return produce_photo_page("2025_07_03")
+
 
 @app.route("/2025_02_08/")
 def photos_2025_02_08():
     """Renders photo page for 2025_02_08"""
-    return produce_photo_page(
-        "2025_02_08"
-        
-    )
+    return produce_photo_page("2025_02_08")
+
 
 @app.route("/2025_06_25/")
 def photos_2025_06_25():
     """Renders photo page for 2025_06_25"""
-    return produce_photo_page(
-        "2025_06_25"
-    )
+    return produce_photo_page("2025_06_25")
+
 
 @app.route("/2025_06_25_2/")
 def photos_2025_06_25_2():
     """Renders photo page for 2025_06_25_2"""
-    return produce_photo_page(
-        "2025_06_25_2"
-    )
+    return produce_photo_page("2025_06_25_2")
+
 
 @app.route("/2025_07_22/")
 def photos_2025_07_22():
     """Renders photo page for 2025_07_22"""
-    return produce_photo_page(
-        "2025_07_22"
-    )
+    return produce_photo_page("2025_07_22")
+
 
 @app.route("/2025_09_06/")
 def photos_2025_09_06():
     """Renders photo page for 2025_09_06"""
-    return produce_photo_page(
-        "2025_09_06"
-    )
+    return produce_photo_page("2025_09_06")
+
 
 @app.route("/2026_01_01/")
 def photos_2026_01_01():
     """Renders photo page for 2026_01_01"""
-    return produce_photo_page(
-        "2026_01_01"
-)
+    return produce_photo_page("2026_01_01")
+
 
 @app.route("/2026_03_09/")
 def photos_2026_03_09():
     """Renders photo page for 2026_03_09"""
-    return produce_photo_page(
-        "2026_03_09"
-    )
+    return produce_photo_page("2026_03_09")
+
 
 @app.route("/2022_10_22/")
 def photos_2022_10_22():
     """Renders photo page for 2022_10_22"""
-    return produce_photo_page(
-        "2022_10_22"
-    )
+    return produce_photo_page("2022_10_22")
 
 
 @app.route("/2024_04_22/")
 def photos_2024_04_22():
     """Renders photo page for 2024_04_22"""
-    return produce_photo_page(
-        "2024_04_22"
-    )
+    return produce_photo_page("2024_04_22")
 
 
 @app.route("/2024_07_21/")
 def photos_2024_07_21():
     """Renders photo page for 2024_07_21"""
-    return produce_photo_page(
-        "2024_07_21"
-    )
+    return produce_photo_page("2024_07_21")
 
 
 @app.route("/2024_08_06/")
 def photos_2024_08_06():
     """Renders photo page for 2024_08_06"""
-    return produce_photo_page(
-        "2024_08_06"
-    )
+    return produce_photo_page("2024_08_06")
+
 
 @app.route("/2024_09_27/")
 def photos_2024_09_27():
     """Renders photo page for 2024_09_27"""
-    return produce_photo_page(
-        "2024_09_27"
-    )
+    return produce_photo_page("2024_09_27")
 
 
 @app.route("/2024_12_01/")
 def photos_2024_12_01():
     """Renders photo page for 2024_12_01"""
-    return produce_photo_page(
-        "2024_12_01"
-    )
+    return produce_photo_page("2024_12_01")
 
-def produce_photo_page(
-    photos_folder_name
-):
+
+def produce_photo_page(photos_folder_name):
     """Renders the photo template page for the photos in the given photos_folder_name"""
     photo_folder_name = "images/" + photos_folder_name
     image_list = []
     for file in os.listdir(os.path.join("static", photo_folder_name)):
         if "txt" not in file:
-            image_list.append(photo_folder_name +"/" + file)
+            image_list.append(photo_folder_name + "/" + file)
     with open(
         "static/images/" + photos_folder_name + "/descriptions.txt", encoding="utf-8"
     ) as f:
--- freeze.py
+++ freeze.py
@@ -1,9 +1,10 @@
 """Freezes the personal site app to static files"""
+
 from flask_frozen import Freezer
 from app import app
 
 
 freezer = Freezer(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     freezer.freeze()
